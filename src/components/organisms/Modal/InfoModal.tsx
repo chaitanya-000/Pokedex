5dfca6fc263814fd0dba77b258eb3927060f01c9
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-11-30 18:28:51 +0530

    Adds egg type description and glass background

--- src/components/organisms/Modal/InfoModal.tsx
+++ src/components/organisms/Modal/InfoModal.tsx
@@ -13,14 +13,17 @@ const InfoModal = ({
   const [pokemonName, setPokemonName] = useState<string>();
   const [pokemonType, setPokemonType] = useState([]);
   const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const [eggType, setEggGroup] = useState([]);
 
-  const getimgSrc = async () => {
+  const dataFromApi = async () => {
     await axios
       .get(`https://pokeapi.co/api/v2/pokemon/${selectedIndex}`)
       .then((response) => {
         setPokemonType(response.data.types);
         setImageApi(response.data?.sprites?.other?.dream_world?.front_default);
         setPokemonName(response.data?.name);
+        setAbilities(response.data.abilities);
       });
   };
 
@@ -28,6 +31,7 @@ const InfoModal = ({
     await axios
       .get(`https://pokeapi.co/api/v2/pokemon-species/${selectedIndex}`)
       .then((response) => {
+        setEggGroup(response.data.egg_groups);
         const filteredFlavourTextArray =
           response.data.flavor_text_entries.filter((eachObj: any) => {
             return eachObj.language.name === "en";
@@ -36,7 +40,6 @@ const InfoModal = ({
         setPokemonInfo(filteredFlavourTextArray);
       });
   };
-
   const arrayofDuplicateTexts = pokemonInfo.map((eachObj: any) => {
     return eachObj.flavor_text;
   });
@@ -46,17 +49,11 @@ const InfoModal = ({
       return arrayofDuplicateTexts.indexOf(eachElement) === index;
     }
   );
-  console.log(
-    filteredArray.map((eachObject) => {
-      console.log(eachObject);
-    })
-  );
 
   useEffect(() => {
-    openModal && getimgSrc();
+    openModal && dataFromApi();
     infoText();
   }, [openModal]);
-
   return (
     openModal && (
       <Modal
@@ -68,33 +65,71 @@ const InfoModal = ({
         closeOnClickOutside
         size={"60%"}
         radius="lg"
+        sx={{
+          background: "rgba(255, 255, 255, 0.2)",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(5px)",
+          border: " 1px solid rgba(255, 255, 255, 0.3)",
+        }}
       >
-        <div className="modal_container">
-          <h3 style={{ border: "2px solid green" }}>
+        <div
+          className="modal_container"
+          style={{
+            background: "rgba(255, 255, 255, 0.2)",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(5px)",
+            border: " 1px solid rgba(255, 255, 255, 0.3)",
+          }}
+        >
+          <h2>
             {selectedIndex}: {pokemonName?.toUpperCase()}
-          </h3>
+          </h2>
 
           <div className="modal_Image_and_info">
             <img
               src={imageapi && imageapi}
               alt=""
-              style={{ width: "20%", border: "2px solid red" }}
+              style={{ width: "20%" }}
+              className="img"
             />
             <div className="info">
-              {}
               <div className="pokemon_type">
                 <h2>Type</h2>
                 {pokemonType.map(
                   (eachObj: { type: { name: string } }, index: number) => {
-                    return <h3 key={index}>{eachObj.type.name}</h3>;
+                    return <li key={index}>{eachObj.type.name}</li>;
                   }
                 )}
+              </div>
+              <div
+                className="pokemon_abilities"
+                // style={{ border: "3px dotted blue" }}
+              >
+                <h2>Abilities</h2>
+                {abilities.map(
+                  (
+                    eachObj: {
+                      ability: {
+                        name: string;
+                      };
+                    },
+                    index: number
+                  ) => {
+                    return <li key={index}>{eachObj.ability.name}</li>;
+                  }
+                )}
+              </div>
+              <div className="pokemon_eggType">
+                <h2>Egg Type</h2>
+                {eggType.map((eachObj: { name: string }, index) => {
+                  return <li key={index}>{eachObj.name}</li>;
+                })}
               </div>
             </div>
           </div>
           <div className="bottom_container">
-            {filteredArray.map((eachObject) => {
-              return <span style={{}}>{eachObject}</span>;
+            {filteredArray.map((eachObject, index) => {
+              return <span key={index}>{eachObject}</span>;
             })}
           </div>
         </div>
